dd66b1aa7a9f7f8c79cbad4d2ce7d8f443898c09
Sindhuula Selvaraju <sselvar4@jhu.edu>
2016-03-22 16:52:24 -0400

    slightly better

--- assignment4/evaluator/symstem.py
+++ assignment4/evaluator/symstem.py
@@ -9,7 +9,9 @@ import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 def findSyn(sentence):
+    print "findsyn"
     synlist = []
+    print "sentence", sentence
     for word in sentence:
 #     try:
         for synset in wordnet.synsets(word):
@@ -17,6 +19,9 @@ def findSyn(sentence):
                 if syn.decode('utf8') not in synlist:
                     synlist.append(syn.decode('utf8'))
  #    except UnicodeDecodeError
+        if word not in synlist:
+            synlist.append(word)
+    print "synlist",synlist
     return synlist
 def word_matches(h, ref):
     return sum(1 for w in h if w in ref)
@@ -29,7 +34,7 @@ def normalise(word):
 
     word = word.lower()
     word = stemmer.stem_word(word)
-  #  word = lemmatizer.lemmatize(word)
+    word = lemmatizer.lemmatize(word)
     return word 
 
 def main():
@@ -50,8 +55,11 @@ def main():
     # note: the -n option does not work in the original code
     for h1, h2, ref in islice(sentences(), opts.num_sentences):
         rset = set(ref)
+        print "rseta",rset
         h1_match = word_matches(h1, rset)
+        print "rsetb",rset
         h2_match = word_matches(h2, rset)
+        print "rsetc",rset
         result = (1 if h1_match > h2_match else # \begin{cases}
                 (0 if h1_match == h2_match
                     else -1)) # \end{cases}
@@ -60,8 +68,11 @@ def main():
     a = 0.1
     for h1, h2, ref in islice(sentences(), opts.num_sentences):
         rset = set(ref)
+        print "rset1",rset
         h1_match = meteor(h1,rset,a) 
+        print "rset2",rset
         h2_match = meteor(h2,rset,a)
+        print "rset3",rset
         result = (1 if h1_match > h2_match else # \begin{cases}
                 (0 if h1_match == h2_match
                     else -1)) # \end{cases}
@@ -73,10 +84,11 @@ def meteor(h,e,a):
     try:
         l = (precision*recall) / (((1-a)*recall) + a*precision)
     except ZeroDivisionError:
-        l = 0
+        l = -1
     return (l)
 
 def findPrecisionRecall(h,e):
+    print "find ",e
     countcommon = 0
     sizeh = len(h)
     sizee = len(e)
@@ -84,8 +96,9 @@ def findPrecisionRecall(h,e):
     synstem = []
     for word in synset:
         synstem.append(normalise(word.decode('utf8')))
-    for i in synstem:
-        e.add(i)
+#  for i in synstem:
+ #       e.add(i)
+    print synstem
     for word in h:
         if normalise(word.decode('utf8')) in synstem:
             countcommon += 1
