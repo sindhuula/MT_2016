e79940c34d87692112452fc1a00409d89c9e82b6
shalarn1 <shalarn1@jhu.edu>
2016-05-10 12:07:13 -0400

    fixing bug

--- finalproject/Code/reorder.py
+++ finalproject/Code/reorder.py
@@ -15,7 +15,7 @@ PARSER = argparse.ArgumentParser(description="Reorder a set of sentences")
 PARSER.add_argument("-t", type=str, default="data/train", help="training data prefix")
 PARSER.add_argument("-d", type=str, default="data/dev", help="dev data prefix")
 PARSER.add_argument("-w", type=str, default="es", help="spanish file")
-PARSER.add_argument("-p", type=str, default="xlsx", help="tag file")
+PARSER.add_argument("-p", type=str, default="data/postags.xlsx", help="tag file")
 args = PARSER.parse_args()
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
@@ -54,7 +54,7 @@ if __name__ == '__main__':
         #Incorrect_ordering = word, dep_tag pair
         #Correct ordering = word, dep_tag pair
         wb = openpyxl.load_workbook(args.p)
-        parsed = wb.get_sheet_by_name(args.t)
+        parsed = wb.get_sheet_by_name("train")
         sentence_no = 0
         order = []
         sentences = defaultdict(defaultdict)
@@ -70,8 +70,8 @@ if __name__ == '__main__':
                 sentences[sentence_no] = word_props
 
         #Dev Section
-        wb = openpyxl.load_workbook(combine(args.d,args.p))
-        parsed = wb.get_sheet_by_name('first_1000')
+        wb = openpyxl.load_workbook(args.p)
+        parsed = wb.get_sheet_by_name('dev')
         sentence_no = 0
         order = []
         sentences = defaultdict(defaultdict)
@@ -79,12 +79,14 @@ if __name__ == '__main__':
         rows = int( parsed.get_highest_row())
         columns = int(parsed.get_highest_column())
         print rows,columns
+        word_props = defaultdict(list)
         for i in range(1, rows+1):
+            word_props[parsed.cell(column = 2, row = i).value] =  [parsed.cell(column = 4, row = i).value, parsed.cell(column = 5, row = i).value,parsed.cell(column = 8, row = i).value]
             if parsed.cell(column = 1, row = i).value == 1 :
-                word_props = defaultdict(list)
-                sentence_no +=1
-                word_props[parsed.cell(column = 2, row = i).value] =  [parsed.cell(column = 4, row = i).value, parsed.cell(column = 5, row = i).value,parsed.cell(column = 8, row = i).value]
                 sentences[sentence_no] = word_props
+                sentence_no +=1
+                word_props = defaultdict(list)
+
         verb_phrase = ""
         subj_phrase = ""
         obj_phrase = ""
@@ -92,29 +94,32 @@ if __name__ == '__main__':
         subjects = ["SUBJ"]
         objects = ["DO","IO","OBLC"]
         verbs = ["v"]
-        sent_no = 0
-        for hypothesis1, hypothesis2, original_sentence in islice(sentences()):
-            sent_no += 1
-            final_sent = ""
-            temp_phrase = []
-            words = sentences[sent_no]
-            for word in words:
-                if word != None:
-                    if (type(word) == long) | (type(word) == float) | (type(word) == int):
-                        word = unicode(str(word),"utf-8")
-                    temp_phrase.append(word)
+        print sentences[2]
+       #words = sentences[1]
+       # print words[0]
+        #print words['Eso'][2]
+        for i in range(1, 3):
+             final_sent = ""
+             temp_phrase = []
+             words = sentences[i]
+            # for word in words:
+            #    print word[2]
+            #     if word != None:
+            #         if (type(word) == long) | (type(word) == float) | (type(word) == int):
+            #             word = unicode(str(word),"utf-8")
+            #         temp_phrase.append(word)
 
-                if word[2] in subjects:
-                    subj_phrase = ' '.join(temp_phrase)
-                    temp_phrase = []
-                elif word[2] in objects:
-                    obj_phrase = ' '.join(temp_phrase)
-                    temp_phrase = []
-                elif word[0] in verbs:
-                    verb_phrase = ' '.join(temp_phrase)
-                    temp_phrase = []
-            final_sent = subj_phrase+' '+verb_phrase+' '+obj_phrase
-            print compare(hypothesis1,hypothesis2,final_sent)
+            #     if word[2] in subjects:
+            #         subj_phrase = ' '.join(temp_phrase)
+            #         temp_phrase = []
+            #     elif word[2] in objects:
+            #         obj_phrase = ' '.join(temp_phrase)
+            #         temp_phrase = []
+            #     elif word[0] in verbs:
+            #         verb_phrase = ' '.join(temp_phrase)
+            #         temp_phrase = []
+            # final_sent = subj_phrase+' '+verb_phrase+' '+obj_phrase
+            # print final_sent
 
             #hyp1 = h1
             #hyp2 = h2
@@ -124,9 +129,9 @@ if __name__ == '__main__':
 
         # From the trained portion find out how the tags were re-ordered and try to replicate
         # Our program simply just exchanges the phrases
-        print "Read the sentences"
-        print "Read the tag files for the sentences"
-        print "Shuffle phrases"
-        print "Train set may actually help to identify the correct ordering of the words and give an idea about how to reshuffle the tags"
-        print "Dev and test set need to use the training model to see how to get an output"
+       # print "Read the sentences"
+       # print "Read the tag files for the sentences"
+       # print "Shuffle phrases"
+       # print "Train set may actually help to identify the correct ordering of the words and give an idea about how to reshuffle the tags"
+       # print "Dev and test set need to use the training model to see how to get an output"
 
